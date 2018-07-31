c9994d69b3ea339d5fe7475685a821304a4d7ebc
Elena Sinianskaya <Elena.Sinianskaya@gmail.com>
2018-07-31 16:27:54 +0300

    Make Menu for a middle screen

--- js/script.js
+++ js/script.js
@@ -4,4 +4,11 @@ $(document).ready(function(){
 		dots:true
 	});
 
+	$('.icon-menu').on('click', function () {
+        $('.header-nav').addClass('active');
+    });
+
+    $('.icon-cancel').on('click', function () {
+        $('.header-nav').removeClass('active');
+    })
 });
